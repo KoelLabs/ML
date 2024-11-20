32b371edf2b4ceba2578fdf7386193343d2aa15c
arunasrivastava <aruna-sri@outlook.com>
2024-11-20 07:45:04 +0000

    new files for allo!

--- scripts/ipa_transcription/allo.py
+++ scripts/ipa_transcription/allo.py
@@ -13,8 +13,8 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 from scripts.audio import audio_record_to_file
-
-model = read_recognizer()
+model_name = "eng2102"
+model = read_recognizer(model_name)
 
 
 def allosaurus_transcribe(input_path):
