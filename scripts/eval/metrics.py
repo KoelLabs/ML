5f6f43ecbe081a6650f448c5db3091ea7d2dd48f
Alexander Metzger <alexander.le@outlook.dk>
2025-03-04 12:08:01 -0800

    properly normalizer FER

--- scripts/eval/metrics.py
+++ scripts/eval/metrics.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+from yaml import warnings
+
+warnings({"YAMLLoadWarning": False})
+
 import panphon
 import panphon.distance
 
+ft = panphon.FeatureTable()
 panphon_dist = panphon.distance.Distance()
+inverse_double_weight_sum = 1 / (sum(ft.weights) * 2)
 
 
 def per(prediction, ground_truth):
@@ -18,6 +28,33 @@ def fer(prediction, ground_truth):
     """
     Feature Error Rate: the edits weighted by their acoustic features summed up and divided by the length of the ground truth.
     """
-    return panphon_dist.weighted_feature_edit_distance(ground_truth, prediction) / len(
-        ground_truth
+    return (
+        inverse_double_weight_sum
+        * panphon_dist.weighted_feature_edit_distance(ground_truth, prediction)
+        / len(ground_truth)
     )
+
+
+def usage():
+    print("Usage: python ./scripts/eval/metrics.py <per|fer> <predicted> <label>")
+    return
+
+
+def main(args):
+    if len(args) < 3:
+        usage()
+        return
+    metric = args[0]
+    predicted = args[1]
+    label = args[2]
+    if metric == "per":
+        print(per(predicted, label))
+    elif metric == "fer":
+        print(fer(predicted, label))
+    else:
+        usage()
+        return
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
