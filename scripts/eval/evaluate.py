fa7d387f99f920ad95145a00b4f4977c4aa8c37a
Alexander Metzger <alexander.le@outlook.dk>
2025-03-11 21:25:08 -0700

    eval cli, more nuanced preprocessing

--- scripts/eval/evaluate.py
+++ scripts/eval/evaluate.py
@@ -3,24 +3,66 @@
 import os
 import sys
 
-from .metrics import fer, per
-
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from core.ipa import filter_chars
+from eval.metrics import fer, per
+from forced_alignment.common import IPA_SYMBOLS, group_phonemes
+
+
+def simplify_ipa(ipa_string):
+    # remove all whitespace and numbers
+    ipa_string = "".join(c for c in ipa_string if not c.isspace() and not c.isdigit())
+    # remove anything that is not a vowel/consonant including tie bars
+    return filter_chars(ipa_string, "letters_rmv_tie")
 
 
 def preprocess_ipa(ipa_string):
-    # remove all whitespace and numbers
-    ipa_string = "".join(c for c in ipa_string if not c.isspace() and not c.isdigit())
-    return filter_chars(ipa_string, "letters_rmv_tie")
+    # remove white space and phonemes not in panphon (IPA_SYMBOLS)
+    phonemes = group_phonemes(ipa_string.replace(" ", ""))
+    unsupported_phonemes = set(phonemes) - set(IPA_SYMBOLS)
+    if len(unsupported_phonemes) > 0:
+        print(
+            f"Warning: unsupported phonemes found in input: {unsupported_phonemes}.",
+            file=sys.stderr,
+        )
+    return "".join(p for p in phonemes if p in IPA_SYMBOLS)
 
 
 def evaluate(label, predicted):
     label_sequence = preprocess_ipa(label)
     pred_sequence = preprocess_ipa(predicted)
-    # TODO: check vocab compatibility, show warning
 
     fer_score = fer(pred_sequence, label_sequence)
     per_score = per(predicted, label)
 
     return per_score, fer_score
+
+
+def usage():
+    print("Usage: python ./scripts/eval/evaluate.py eval <label> <predicted>")
+    print("Usage: python ./scripts/eval/evaluate.py process <ipa_string>")
+
+
+def main(args):
+    if len(args) < 2:
+        usage()
+        return
+    command = args[0]
+    if command == "eval":
+        if len(args) < 3:
+            usage()
+            return
+        label = args[1]
+        predicted = args[2]
+        per_score, fer_score = evaluate(label, predicted)
+        print(f"PER: {per_score}")
+        print(f"FER: {fer_score}")
+    elif command == "process":
+        ipa_string = args[1]
+        print(preprocess_ipa(ipa_string))
+    else:
+        usage()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
