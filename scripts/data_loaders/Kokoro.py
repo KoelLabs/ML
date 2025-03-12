b8c700a63196f67116e4bf1b3dce974383b13a43
Alexander Metzger <alexander.le@outlook.dk>
2025-03-11 21:19:43 -0700

    refactor kokoro data loading code (and fix missing dependency and download code)

---
+++ scripts/data_loaders/Kokoro.py
@@ -0,0 +1,119 @@
+import os
+import sys
+
+import zipfile
+from misaki import ja
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from data_loaders.common import BaseDataset
+from core.audio import audio_bytes_to_array
+from forced_alignment.common import group_phonemes
+
+SOURCE_SAMPLE_RATE = 22050
+DATA_ZIP = os.path.join(
+    os.path.dirname(__file__), "..", "..", ".data", "Kokoro-Speech-Dataset.zip"
+)
+SUBSTITUTE = {
+    "ʦ": "ts",
+    "ʨ": "tʃ",
+}
+
+g2p = ja.JAG2P()
+
+
+class KokoraDataset(BaseDataset):
+    def __init__(
+        self,
+        split="meian-by-soseki-natsume",
+        include_timestamps=False,
+        include_speaker_info=False,
+    ):
+        super().__init__(split, include_timestamps, include_speaker_info)
+
+        if include_timestamps:
+            raise NotImplementedError("Timestamps are available but not parsed yet.")
+
+        metadata_zip = zipfile.ZipFile(DATA_ZIP, "r")
+        splits = [
+            s[: -len(".metadata.txt")]
+            for s in metadata_zip.namelist()
+            if s.endswith(".metadata.txt")
+        ]
+        assert split in splits, f"Split {split} not found in {splits}"
+
+        with metadata_zip.open(f"{split}.metadata.txt", "r") as metadata_file:
+            self.metadata = metadata_file.read().decode("utf-8").split("\n")
+            self.metadata = [
+                {
+                    "file": x.split("|")[1],  # wav filename
+                    "start": x.split("|")[2],
+                    "end": x.split("|")[3],
+                    "transcription": x.split("|")[4],  # Kanji-kana mixture text
+                    "reading": x.split("|")[5],  # Romanized text
+                }
+                for x in self.metadata
+                if len(x.split("|")) > 5
+            ]
+
+        metadata_zip.close()
+
+        self.zip = zipfile.ZipFile(
+            os.path.join(
+                os.path.dirname(__file__),
+                "..",
+                "..",
+                ".data",
+                "Kokoro-Speech-Dataset",
+                f"{split}.zip",
+            ),
+            "r",
+        )
+        self.files = self.zip.namelist()
+
+    def __del__(self):
+        self.zip.close()
+
+    def __len__(self):
+        return len(self.metadata)
+
+    def _get_ix(self, ix):
+        metadata = self.metadata[ix]
+        filename = metadata["file"]
+        with self.zip.open(filename) as wav_file:
+            audio = audio_bytes_to_array(wav_file.read(), SOURCE_SAMPLE_RATE)
+
+        transcript = (
+            metadata["transcription"]
+            .replace(" ", "")
+            .replace("。", "")
+            .replace("、", "")
+            .replace("…", "")
+        )
+        phonemes, tokens = g2p(transcript)
+        phonemes = (
+            phonemes.replace(" ", "")
+            .replace("“", "")
+            .replace("``", "")
+            .replace("”", "")
+        )
+        for key, value in SUBSTITUTE.items():
+            phonemes = phonemes.replace(key, value)
+        phonemes = group_phonemes(phonemes)
+        ipa = "".join(phonemes)
+
+        if self.include_speaker_info:
+            speaker = {
+                "reader": self.split.split("-by-")[0],
+                "author": self.split.split("-by-")[1],
+                "transcription": metadata["transcription"],
+                "reading": metadata["reading"],
+            }
+            return ipa, audio, speaker
+        else:
+            return ipa, audio
+
+
+if __name__ == "__main__":
+    data = KokoraDataset(include_speaker_info=True)
+
+    print(data[0])
--- scripts/install.sh
+++ scripts/install.sh
@@ -52,3 +52,22 @@ curl -L -o ./.data/TIMIT.zip\
 cd ./.data
 wget https://huggingface.co/datasets/declare-lab/MELD/resolve/main/MELD.Raw.tar.gz
 cd ../
+
+# download Kokoro-Speech-Dataset
+curl -L -o ./.data/Kokoro-Speech-Dataset.zip\
+  https://github.com/kaiidams/Kokoro-Speech-Dataset/releases/download/1.3/kokoro-speech-v1_3.zip
+mkdir -p ./.data/Kokoro-Speech-Dataset
+curl -L -o ./.data/Kokoro-Speech-Dataset/meian-by-soseki-natsume.zip https://www.archive.org/download/meian_1403_librivox/meian_1403_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/kokoro-by-soseki-natsume.zip http://www.archive.org/download//kokoro_natsume_um_librivox/kokoro_natsume_um_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/inakakyoshi-by-katai-tayama.zip http://www.archive.org/download//inakakyoshi_1311_librivox/inakakyoshi_1311_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/nowaki-by-soseki-natsume.zip http://www.archive.org/download/nowaki_um_librivox/nowaki_um_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/kusamakura-by-soseki-natsume.zip http://www.archive.org/download//kusamakura_1311_librivox/kusamakura_1311_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/botchan-by-soseki-natsume-2.zip http://www.archive.org/download//botchan_1310_librivox/botchan_1310_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/gan-by-ogai-mori.zip http://www.archive.org/download//gan_1311_librivox/gan_1311_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/umareizuru-nayami-by-takeo-arishima.zip http://www.archive.org/download/umareizuru_nayami_ez_1302/umareizuru_nayami_ez_1302_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/garasudono-uchi-by-natsume-soseki.zip http://www.archive.org/download/garasudonouchi_1208_librivox/garasudonouchi_1208_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/eijitsu-syohin-by-soseki-natsume.zip http://www.archive.org/download/eijitsu_syohin_um_librivox/eijitsu_syohin_um_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/futon-by-katai-tayama.zip http://www.archive.org/download/futon_ek_1303_librivox/futon_ek_1303_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/kouyahijiri-by-kyoka-izumi.zip http://www.archive.org/download/kouyahijiri_1303_librivox/kouyahijiri_1303_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/gongitsune-by-nankichi-niimi.zip http://archive.org/download/gongitsune_um_librivox/gongitsune_um_librivox_64kb_mp3.zip
+curl -L -o ./.data/Kokoro-Speech-Dataset/caucasus-no-hagetaka-by-yoshio-toyoshima.zip http://www.archive.org/download/caucasus_no_hagetaka_um_librivox/caucasus_no_hagetaka_um_librivox_64kb_mp3.zip
