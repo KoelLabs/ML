b825b9ee4a35c71a30f36ac57d5afcf3ca88a935
Alexander Metzger <alexander.le@outlook.dk>
2025-03-15 23:35:49 -0700

    coraal data loading code

--- scripts/install.sh
+++ scripts/install.sh
@@ -75,3 +75,90 @@ curl -L -o ./.data/Kokoro-Speech-Dataset/futon-by-katai-tayama.zip http://www.ar
 curl -L -o ./.data/Kokoro-Speech-Dataset/kouyahijiri-by-kyoka-izumi.zip http://www.archive.org/download/kouyahijiri_1303_librivox/kouyahijiri_1303_librivox_64kb_mp3.zip
 curl -L -o ./.data/Kokoro-Speech-Dataset/gongitsune-by-nankichi-niimi.zip http://archive.org/download/gongitsune_um_librivox/gongitsune_um_librivox_64kb_mp3.zip
 curl -L -o ./.data/Kokoro-Speech-Dataset/caucasus-no-hagetaka-by-yoshio-toyoshima.zip http://www.archive.org/download/caucasus_no_hagetaka_um_librivox/caucasus_no_hagetaka_um_librivox_64kb_mp3.zip
+
+# download CORAAL https://oraal.github.io/coraal
+mkdir -p ./.data/CORAAL/phonemes
+mkdir -p ./.data/CORAAL/audio
+mkdir -p ./.data/CORAAL/transcripts
+curl -L -o ./.data/CORAAL/phonemes/DCA.zip http://lingtools.uoregon.edu/coraal/aligned/DCA_MFA_2019.06.zip
+curl -L -o ./.data/CORAAL/phonemes/DCB.zip http://lingtools.uoregon.edu/coraal/aligned/DCB_MFA_2019.06.zip
+curl -L -o ./.data/CORAAL/phonemes/PRV.zip http://lingtools.uoregon.edu/coraal/aligned/PRV_MFA_2019.06.zip
+curl -L -o ./.data/CORAAL/phonemes/ROC.zip http://lingtools.uoregon.edu/coraal/aligned/ROC_MFA_2019.06.zip
+
+curl -L -o ./.data/CORAAL/ATL-metadata.txt http://lingtools.uoregon.edu/coraal/atl/2020.05/ATL_metadata_2020.05.txt
+curl -L -o ./.data/CORAAL/audio/ATL_1.tar.gz http://lingtools.uoregon.edu/coraal/atl/2020.05/ATL_audio_part01_2020.05.tar.gz
+curl -L -o ./.data/CORAAL/audio/ATL_2.tar.gz http://lingtools.uoregon.edu/coraal/atl/2020.05/ATL_audio_part02_2020.05.tar.gz
+curl -L -o ./.data/CORAAL/audio/ATL_3.tar.gz http://lingtools.uoregon.edu/coraal/atl/2020.05/ATL_audio_part03_2020.05.tar.gz
+curl -L -o ./.data/CORAAL/audio/ATL_4.tar.gz http://lingtools.uoregon.edu/coraal/atl/2020.05/ATL_audio_part04_2020.05.tar.gz
+curl -L -o ./.data/CORAAL/transcripts/ATL.tar.gz http://lingtools.uoregon.edu/coraal/atl/2020.05/ATL_textfiles_2020.05.tar.gz
+
+curl -L -o ./.data/CORAAL/DCA-metadata.txt http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_metadata_2018.10.06.txt
+curl -L -o ./.data/CORAAL/audio/DCA_1.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part01_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCA_2.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part02_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCA_3.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part03_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCA_4.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part04_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCA_5.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part05_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCA_6.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part06_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCA_7.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part07_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCA_8.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part08_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCA_9.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part09_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCA_10.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_audio_part10_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/transcripts/DCA.tar.gz http://lingtools.uoregon.edu/coraal/dca/2018.10.06/DCA_textfiles_2018.10.06.tar.gz
+
+curl -L -o ./.data/CORAAL/DCB-metadata.txt http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_metadata_2018.10.06.txt
+curl -L -o ./.data/CORAAL/audio/DCB_1.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part01_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_2.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part02_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_3.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part03_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_4.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part04_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_5.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part05_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_6.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part06_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_7.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part07_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_8.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part08_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_9.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part09_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_10.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part10_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_11.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part11_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_12.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part12_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_13.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part13_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DCB_14.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_audio_part14_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/transcripts/DCB.tar.gz http://lingtools.uoregon.edu/coraal/dcb/2018.10.06/DCB_textfiles_2018.10.06.tar.gz
+
+curl -L -o ./.data/CORAAL/DTA-metadata.txt http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_metadata_2023.06.txt
+curl -L -o ./.data/CORAAL/audio/DTA_1.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part01_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DTA_2.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part02_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DTA_3.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part03_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DTA_4.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part04_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DTA_5.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part05_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DTA_6.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part06_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DTA_7.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part07_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DTA_8.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part08_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DTA_9.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part09_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/DTA_10.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_audio_part10_2023.06.tar.gz
+curl -L -o ./.data/CORAAL/transcripts/DTA.tar.gz http://lingtools.uoregon.edu/coraal/dta/2023.06/DTA_textfiles_2023.06.tar.gz
+
+curl -L -o ./.data/CORAAL/LES-metadata.txt http://lingtools.uoregon.edu/coraal/les/2021.07/LES_metadata_2021.07.txt
+curl -L -o ./.data/CORAAL/audio/LES_1.tar.gz http://lingtools.uoregon.edu/coraal/les/2021.07/LES_audio_part01_2021.07.tar.gz
+curl -L -o ./.data/CORAAL/audio/LES_2.tar.gz http://lingtools.uoregon.edu/coraal/les/2021.07/LES_audio_part02_2021.07.tar.gz
+curl -L -o ./.data/CORAAL/audio/LES_3.tar.gz http://lingtools.uoregon.edu/coraal/les/2021.07/LES_audio_part03_2021.07.tar.gz
+curl -L -o ./.data/CORAAL/transcripts/LES.tar.gz http://lingtools.uoregon.edu/coraal/les/2021.07/LES_textfiles_2021.07.tar.gz
+
+curl -L -o ./.data/CORAAL/PRV-metadata.txt http://lingtools.uoregon.edu/coraal/prv/2018.10.06/PRV_metadata_2018.10.06.txt
+curl -L -o ./.data/CORAAL/audio/PRV_1.tar.gz http://lingtools.uoregon.edu/coraal/prv/2018.10.06/PRV_audio_part01_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/PRV_2.tar.gz http://lingtools.uoregon.edu/coraal/prv/2018.10.06/PRV_audio_part02_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/PRV_3.tar.gz http://lingtools.uoregon.edu/coraal/prv/2018.10.06/PRV_audio_part03_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/audio/PRV_4.tar.gz http://lingtools.uoregon.edu/coraal/prv/2018.10.06/PRV_audio_part04_2018.10.06.tar.gz
+curl -L -o ./.data/CORAAL/transcripts/PRV.tar.gz http://lingtools.uoregon.edu/coraal/prv/2018.10.06/PRV_textfiles_2018.10.06.tar.gz
+
+curl -L -o ./.data/CORAAL/ROC-metadata.txt http://lingtools.uoregon.edu/coraal/roc/2020.05/ROC_metadata_2020.05.txt
+curl -L -o ./.data/CORAAL/audio/ROC_1.tar.gz http://lingtools.uoregon.edu/coraal/roc/2020.05/ROC_audio_part01_2020.05.tar.gz
+curl -L -o ./.data/CORAAL/audio/ROC_2.tar.gz http://lingtools.uoregon.edu/coraal/roc/2020.05/ROC_audio_part02_2020.05.tar.gz
+curl -L -o ./.data/CORAAL/audio/ROC_3.tar.gz http://lingtools.uoregon.edu/coraal/roc/2020.05/ROC_audio_part03_2020.05.tar.gz
+curl -L -o ./.data/CORAAL/audio/ROC_4.tar.gz http://lingtools.uoregon.edu/coraal/roc/2020.05/ROC_audio_part04_2020.05.tar.gz
+curl -L -o ./.data/CORAAL/audio/ROC_5.tar.gz http://lingtools.uoregon.edu/coraal/roc/2020.05/ROC_audio_part05_2020.05.tar.gz
+curl -L -o ./.data/CORAAL/transcripts/ROC.tar.gz http://lingtools.uoregon.edu/coraal/roc/2020.05/ROC_textfiles_2020.05.tar.gz
+
+curl -L -o ./.data/CORAAL/VLD-metadata.txt http://lingtools.uoregon.edu/coraal/vld/2021.07/VLD_metadata_2021.07.txt
+curl -L -o ./.data/CORAAL/audio/VLD_1.tar.gz http://lingtools.uoregon.edu/coraal/vld/2021.07/VLD_audio_part01_2021.07.tar.gz
+curl -L -o ./.data/CORAAL/audio/VLD_2.tar.gz http://lingtools.uoregon.edu/coraal/vld/2021.07/VLD_audio_part02_2021.07.tar.gz
+curl -L -o ./.data/CORAAL/audio/VLD_3.tar.gz http://lingtools.uoregon.edu/coraal/vld/2021.07/VLD_audio_part03_2021.07.tar.gz
+curl -L -o ./.data/CORAAL/audio/VLD_4.tar.gz http://lingtools.uoregon.edu/coraal/vld/2021.07/VLD_audio_part04_2021.07.tar.gz
+curl -L -o ./.data/CORAAL/transcripts/VLD.tar.gz http://lingtools.uoregon.edu/coraal/vld/2021.07/VLD_textfiles_2021.07.tar.gz
