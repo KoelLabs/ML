7ce47f4c759098612dfcb7eaad83f0776848d286
Alexander Metzger <alexander.le@outlook.dk>
2025-04-10 23:49:19 -0700

    improve google cloud speech recognition to handle longer files

--- scripts/asr/google_speech.py
+++ scripts/asr/google_speech.py
@@ -7,7 +7,8 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from core.audio import audio_record_to_array, audio_array_to_bytes
+from core.gcs import get_gcs_uri, create_temp_object_from_bytes
+from core.audio import audio_record_to_array, audio_array_to_bytes, TARGET_SAMPLE_RATE
 from core.load_secrets import load_secrets
 
 load_secrets()
@@ -16,33 +17,62 @@ load_secrets()
 client = SpeechClient()
 
 
-def google_transcribe_from_bytes(input_bytes):
+def google_transcribe_from_bytes(input_bytes, longer_than_one_minute=True, timeout=120):
     config = cloud_speech.RecognitionConfig(
         auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
         language_codes=["en-US"],
-        model="short",  # Chosen model
+        model="long" if longer_than_one_minute else "short",  # Chosen model
+    )
+    recognizer = (
+        f"projects/{os.environ.get('GOOGLE_PROJECT_ID')}/locations/global/recognizers/_"
     )
 
-    request = cloud_speech.RecognizeRequest(
-        recognizer=f"projects/{os.environ.get('GOOGLE_PROJECT_ID')}/locations/global/recognizers/_",
-        config=config,
-        content=input_bytes,
-    )
+    if longer_than_one_minute:
+        with create_temp_object_from_bytes(input_bytes) as key:
+            url = get_gcs_uri(key)
+            file_metadata = cloud_speech.BatchRecognizeFileMetadata(
+                uri=url,
+            )
 
-    # Transcribes the audio into text
-    return client.recognize(request=request)
+            request = cloud_speech.BatchRecognizeRequest(
+                recognizer=recognizer,
+                config=config,
+                files=[file_metadata],
+                recognition_output_config=cloud_speech.RecognitionOutputConfig(
+                    inline_response_config=cloud_speech.InlineOutputConfig(),
+                ),
+            )
+
+            operation = client.batch_recognize(request=request)
+            response = operation.result(timeout=timeout)
+            return response.results[url].transcript  # type: ignore
+    else:
+        request = cloud_speech.RecognizeRequest(
+            recognizer=recognizer,
+            config=config,
+            content=input_bytes,
+        )
+
+        # Transcribes the audio into text
+        return client.recognize(request=request)
 
 
-def google_transcribe_from_file(input_path):
+def google_transcribe_from_file(input_path, longer_than_one_minute=True, timeout=120):
     with open(input_path, "rb") as f:
         input_bytes = f.read()
 
-    return google_transcribe_from_bytes(input_bytes)
+    return google_transcribe_from_bytes(
+        input_bytes, longer_than_one_minute=longer_than_one_minute, timeout=timeout
+    )
 
 
-def google_transcribe_from_array(input_array):
+def google_transcribe_from_array(input_array, min_timeout=120):
+    duration = len(input_array) / TARGET_SAMPLE_RATE
+    timeout = int(max(min_timeout, duration * 2))
     input_bytes = audio_array_to_bytes(input_array)
-    return google_transcribe_from_bytes(input_bytes)
+    return google_transcribe_from_bytes(
+        input_bytes, longer_than_one_minute=duration > 60, timeout=timeout
+    )
 
 
 def google_transcribe_from_mic():
@@ -53,13 +83,13 @@ def google_transcribe_from_mic():
 def main(args):
     if args[0] == "mic":
         response = google_transcribe_from_mic()
-        for result in response.results:
+        for result in response.results:  # type: ignore
             print(f"Transcript: {result.alternatives[0].transcript}")
     else:
         try:
             input_path = args[0]
             response = google_transcribe_from_file(input_path)
-            for result in response.results:
+            for result in response.results:  # type: ignore
                 print(f"Transcript: {result.alternatives[0].transcript}")
         except Exception as e:
             print(e)
