3c11c28504b7977f9dcbc5fe997d7cda18cc0416
arunasrivastava <aruna-sri@outlook.com>
2025-05-24 12:48:56 -0700

    remove stress marker, delet print

--- scripts/core/ipa.py
+++ scripts/core/ipa.py
@@ -46,7 +46,6 @@ def remove_tones_and_stress(ipa_string: str):
 
 def remove_stress_marker(ipa_string: str):
     """Remove stress marker from the IPA string"""
-    print("we are removing stress marker")
     ipa_string = ipa_string.replace("ˈ", "").replace("ˌ", "")
     return ipa_string
 
