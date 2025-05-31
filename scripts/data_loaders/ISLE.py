fb7ee7668b09e1ffca5aea01b7548becb8668f20
arunasrivastava <aruna-sri@outlook.com>
2025-05-30 18:10:02 -0700

    basic parsing for clab files, vocab mapping needs correction

--- scripts/data_loaders/ISLE.py
+++ scripts/data_loaders/ISLE.py
@@ -1,4 +1,4 @@
-# ISLE Speech corpus details here: https://catalogue.elra.info/en-us/repository/browse/ELRA-S0083/ 
+# ISLE Speech corpus details here: https://catalogue.elra.info/en-us/repository/browse/ELRA-S0083/
 # paper: http://www.lrec-conf.org/proceedings/lrec2000/pdf/313.pdf
 
 import os
@@ -19,9 +19,11 @@ from core.codes import isle2ipa, IPA2ARPABET
 
 
 SOURCE_SAMPLE_RATE = 16_000
-DATA_ZIP= os.path.join(os.path.dirname(__file__), "..", "..", ".data", "S0083.zip")
+DATA_ZIP = os.path.join(os.path.dirname(__file__), "..", "..", ".data", "S0083.zip")
 
 MISSING = []
+
+
 class ISLE(BaseDataset):
     """
     Each speaker in the SPEAKERS dictionary is a valid split.
@@ -29,11 +31,12 @@ class ISLE(BaseDataset):
     by (mostly) intermediate-level German and Italian learners of
     English. ISLEDAT1/ISLEDAT2 contain 23 German sessions, ISLEDAT3/ISLEDAT4 contain 23 Italian sessions.
     """
+
     def __init__(
         self,
         split="SESS0006",
         include_timestamps=False,
-        include_text= False,
+        include_text=False,
         include_speaker_info=False,
     ):
         super().__init__(split, include_timestamps, include_speaker_info)
@@ -42,28 +45,28 @@ class ISLE(BaseDataset):
         self.files = []
 
         if split == "all":
-            splits =SPEAKERS["GERMAN"] + SPEAKERS["ITALIAN"]
+            splits = SPEAKERS["GERMAN"] + SPEAKERS["ITALIAN"]
         elif split == "german":
             splits = SPEAKERS["GERMAN"]
         elif split == "italian":
             splits = SPEAKERS["ITALIAN"]
         else:
             splits = [split]
-        
+
         for sub_dir in sub_directories:
             for name in self.isle.namelist():
                 for session in splits:
                     if f"{sub_dir}/{session}/CLABS" in name and name.endswith(".LAB"):
                         base = os.path.basename(name).replace(".LAB", "")
-                        full_sub_dir = name.split("/")[0] + "/" + sub_dir   
+                        full_sub_dir = name.split("/")[0] + "/" + sub_dir
                         self.files.append((full_sub_dir, session, base))
-
 
         self.vocab = set(IPA2ARPABET.keys())
 
     def __del__(self):
         if hasattr(self, "isle"):
             self.isle.close()
+
     def __len__(self):
         return len(self.files)
 
@@ -97,7 +100,6 @@ class ISLE(BaseDataset):
                 continue  # or phoneme = "�"
             timestamped_phonemes.append((phoneme, int(start_us), int(end_us)))
 
-
         ipa = "".join([x[0] for x in timestamped_phonemes])
         print(f"Processing {base} with IPA: {ipa}")
 
@@ -110,27 +112,59 @@ class ISLE(BaseDataset):
         if self.include_timestamps:
             result.append(timestamped_phonemes)
         return tuple(result)
-       
-        
-        
+
 
 SPEAKERS = {
     "GERMAN": [
-        "SESS0006", "SESS0011", "SESS0012", "SESS0015",
-        "SESS0020", "SESS0021", "SESS0161", "SESS0162",
-        "SESS0163", "SESS0164", "SESS0181", "SESS0182",
-        "SESS0183", "SESS0184", "SESS0185", "SESS0186",
-        "SESS0187", "SESS0188", "SESS0189", "SESS0190",
-        "SESS0191", "SESS0192", "SESS0193",
+        "SESS0006",
+        "SESS0011",
+        "SESS0012",
+        "SESS0015",
+        "SESS0020",
+        "SESS0021",
+        "SESS0161",
+        "SESS0162",
+        "SESS0163",
+        "SESS0164",
+        "SESS0181",
+        "SESS0182",
+        "SESS0183",
+        "SESS0184",
+        "SESS0185",
+        "SESS0186",
+        "SESS0187",
+        "SESS0188",
+        "SESS0189",
+        "SESS0190",
+        "SESS0191",
+        "SESS0192",
+        "SESS0193",
     ],
     "ITALIAN": [
-        "SESS0003", "SESS0040", "SESS0041", "SESS0121",
-        "SESS0122", "SESS0123", "SESS0124", "SESS0125",
-        "SESS0126", "SESS0127", "SESS0128", "SESS0129",
-        "SESS0130", "SESS0131", "SESS0132", "SESS0133",
-        "SESS0134", "SESS0135", "SESS0136", "SESS0137",
-        "SESS0138", "SESS0139", "SESS0140",
-    ]
+        "SESS0003",
+        "SESS0040",
+        "SESS0041",
+        "SESS0121",
+        "SESS0122",
+        "SESS0123",
+        "SESS0124",
+        "SESS0125",
+        "SESS0126",
+        "SESS0127",
+        "SESS0128",
+        "SESS0129",
+        "SESS0130",
+        "SESS0131",
+        "SESS0132",
+        "SESS0133",
+        "SESS0134",
+        "SESS0135",
+        "SESS0136",
+        "SESS0137",
+        "SESS0138",
+        "SESS0139",
+        "SESS0140",
+    ],
 }
 
 
