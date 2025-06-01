5aeefe47b6ffe89dac428da2c5a9cf29e8a24b4a
arunasrivastava <aruna-sri@outlook.com>
2025-05-31 17:34:27 -0700

    update codes for ISLE

--- scripts/core/codes.py
+++ scripts/core/codes.py
@@ -217,29 +217,6 @@ IPA2TIMIT = {v: k for k, v in TIMIT2IPA.items()}
 INVERSE_CLOSURE_INTERVALS = {v: k for k, val in CLOSURE_INTERVALS.items() for v in val}
 
 
-#########################################################################
-# Isle (buckeye + 'oh' mapping)
-ISLE2IPA = {'aa':'ɑ', 'ae':'æ', 'ay':'aɪ', 'aw':'aʊ', 'ao':'ɔ', 'oh':'ɒ', 'oy':'ɔɪ', 'ow':'oʊ', 'eh':'ɛ', 'ey':'eɪ', 'er':'ɝ', 'ah':'ʌ', 'uw':'u', 'uh':'ʊ', 'ih':'ɪ', 'iy':'i', 'm':'m', 'n':'n', 'en':'n̩', 'ng':'ŋ', 'l':'l', 'el':'l̩', 't':'t', 'd':'d', 'ch':'tʃ', 'jh':'dʒ', 'th':'θ', 'dh':'ð', 'sh':'ʃ', 'zh':'ʒ', 's':'s', 'z':'z', 'k':'k', 'g':'ɡ', 'p':'p', 'b':'b', 'f':'f', 'v':'v', 'w':'w', 'hh':'h', 'y':'j', 'r':'ɹ', 'dx':'ɾ', 'nx':'ɾ̃', 'tq':'ʔ', 'er':'ɚ', 'em':'m̩', 'ihn': 'ĩ', 'ehn': 'ɛ̃', 'own': 'oʊ̃', 'ayn': 'aɪ̃', 'aen': 'æ̃', 'aan': 'ɑ̃', 'ahn': 'ə̃', 'eng': 'ŋ̍', 'iyn': 'ĩ', 'uhn': 'ʊ̃'}  # fmt: skip
-IPA2ISLE = {v: k for k, v in ISLE2IPA.items()}
-# 'Vn':'◌̃'
-
-
-def ipa2isle(ipa_string, lang="eng"):
-    update_dict_with_tones(ISLE2IPA, IPA2ISLE, lang)
-    ipa_symbols = string2symbols(ipa_string, IPA2ISLE.keys())[0]
-    isle_symbols = [IPA2ISLE[x] for x in ipa_symbols]
-    return " ".join(isle_symbols)
-
-
-def isle2ipa(isle_string, lang="eng"):
-    update_dict_with_tones(ISLE2IPA, IPA2ISLE, lang)
-    if " " in isle_string:
-        isle_symbols = isle_string.split()
-    else:
-        isle_symbols = string2symbols(isle_string, ISLE2IPA.keys())[0]
-    return "".join([ISLE2IPA[x] for x in isle_symbols])
-
-
 def parse_timit(lines):
     # parses the format of a TIMIT .PHN file, handling edge cases where the closure interval and stops are not always paired
     timestamped_phonemes = []
@@ -299,6 +276,30 @@ def timit2ipa(timit_string, lang="eng"):
         )[0]
     timestamped_phonemes = parse_timit((f"0 0 {x}" for x in timit_symbols))
     return "".join([x[0] for x in timestamped_phonemes])
+
+
+#########################################################################
+# Isle (buckeye + 'oh' mapping) 'ɒ',
+ISLE2IPA = {'AA':'ɑ','AE':'æ','AH':'ʌ','AO':'ɔ','IX':'ɨ','AW':'aʊ','AX': 'ə', 'AXR': 'ɚ','AY':'aɪ','EH':'ɛ','ER':'ɝ','EY':'eɪ','IH':'ɪ','IY':'i','OW':'oʊ','OY':'ɔɪ', 'OH':'ɒ', 'UH':'ʊ','UW':'u','UX':'ʉ','B':'b','CH':'tʃ','D':'d','DH':'ð','EL':'l̩','EM':'m̩','EN':'n̩','F':'f','G':'ɡ','HH':'h','H':'h','JH':'dʒ','K':'k','L':'l','M':'m','N':'n','NG':'ŋ','NX':'ɾ̃','P':'p','Q':'ʔ','R':'ɹ','S':'s','SH':'ʃ','T':'t','TH':'θ','V':'v','W':'w','WH':'ʍ','Y':'j','Z':'z','ZH':'ʒ','DX': 'ɾ',}  # fmt: skip
+
+IPA2ISLE = {v: k for k, v in ISLE2IPA.items()}
+# 'Vn':'◌̃'
+
+
+def ipa2isle(ipa_string, lang="eng"):
+    update_dict_with_tones(ISLE2IPA, IPA2ISLE, lang)
+    ipa_symbols = string2symbols(ipa_string, IPA2ISLE.keys())[0]
+    isle_symbols = [IPA2ISLE[x] for x in ipa_symbols]
+    return " ".join(isle_symbols)
+
+
+def isle2ipa(isle_string, lang="eng"):
+    update_dict_with_tones(ISLE2IPA, IPA2ISLE, lang)
+    if " " in isle_string:
+        isle_symbols = isle_string.split()
+    else:
+        isle_symbols = string2symbols(isle_string, ISLE2IPA.keys())[0]
+    return "".join([ISLE2IPA[x] for x in isle_symbols])
 
 
 #########################################################################
