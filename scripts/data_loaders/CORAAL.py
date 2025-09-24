5573e414efcf523d7b44fde33202e76c3c5db632
SanderGi <alexander.le@outlook.dk>
2025-09-24 00:00:32 -0700

    run asr eval on more CORAAL samples

--- scripts/data_loaders/CORAAL.py
+++ scripts/data_loaders/CORAAL.py
@@ -26,7 +26,7 @@ class CORAALDataset(BaseDataset):
         include_timestamps=False,
         include_speaker_info=False,
         include_text=False,
-        crop_to_first_sample=False,
+        max_samples_per_recording=None,
     ):
         super().__init__(split, include_timestamps, include_speaker_info, include_text)
 
@@ -35,62 +35,14 @@ class CORAALDataset(BaseDataset):
 
         assert split in SPLITS, f"Split {split} not found in {SPLITS}"
 
-        self.metadata = pd.read_csv(
+        metadata = pd.read_csv(
             os.path.join(DATA_BASE_DIR, f"{split}-metadata.txt"), sep="\t"
         )
-        self.crop_to_first_sample = crop_to_first_sample
 
-    def __len__(self):
-        return len(self.metadata)
-
-    def _get_ix(self, ix):
-        row = self.metadata.iloc[ix]
-        subset = row["CORAAL.Sub"]
-        part = int(re.findall(r"\d+", row["Audio.Folder"])[-1])
-        filename = os.path.join(".", row["CORAAL.File"])
-        assert row["Sampling.Rate"] == f"{SOURCE_SAMPLE_RATE / 1000:.1f} kHz"
-
-        with gzip.GzipFile(
-            os.path.join(DATA_BASE_DIR, "transcripts", f"{subset}.tar.gz"), "r"
-        ) as gz:
-            with tarfile.TarFile(fileobj=gz) as tar:
-                with tar.extractfile(filename + ".txt") as txt_file:  # type: ignore
-                    transcript = pd.read_csv(txt_file, sep="\t")
-                    transcript.set_index("Line", inplace=True)
-
-        with gzip.GzipFile(
-            os.path.join(DATA_BASE_DIR, "audio", f"{subset}_{part}.tar.gz"), "r"
-        ) as gz:
-            with tarfile.TarFile(fileobj=gz) as tar:
-                with tar.extractfile(filename + ".wav") as wav_file:  # type: ignore
-                    audio = audio_bytes_to_array(wav_file.read(), SOURCE_SAMPLE_RATE)
-
-        if self.crop_to_first_sample:
-            first_non_empty_ix = next(
-                i
-                for i, p in enumerate(list(transcript["Content"]))
-                if not p.startswith("(")
-            )
-            transcript = transcript.iloc[first_non_empty_ix : first_non_empty_ix + 1]
-            audio = audio[
-                int(transcript.iloc[0]["StTime"] * TARGET_SAMPLE_RATE) : int(
-                    transcript.iloc[0]["EnTime"] * TARGET_SAMPLE_RATE
-                )
-            ]
-
-        result = [transcript, audio]
-        if self.include_speaker_info:
-            result.append(row.to_dict())
-        if self.include_text:
-            result[0] = None
-            result.append(
-                " ".join([p for p in transcript["Content"] if not p.startswith("(")])
-            )
-        return tuple(result)
-
-    def search_transcript(self, query, flags=re.IGNORECASE):
-        for i, row in self.metadata.iterrows():
+        self.samples = []
+        for _, row in metadata.iterrows():
             subset = row["CORAAL.Sub"]
+            part = int(re.findall(r"\d+", row["Audio.Folder"])[-1])
             filename = os.path.join(".", row["CORAAL.File"])
             assert row["Sampling.Rate"] == f"{SOURCE_SAMPLE_RATE / 1000:.1f} kHz"
 
@@ -101,27 +53,65 @@ class CORAALDataset(BaseDataset):
                     with tar.extractfile(filename + ".txt") as txt_file:  # type: ignore
                         transcript = pd.read_csv(txt_file, sep="\t")
                         transcript.set_index("Line", inplace=True)
+                        transcript = transcript[
+                            ~transcript["Spkr"].str.contains("_int_")
+                            & ~transcript["Content"].str.startswith("(")
+                            & ~transcript["Content"].str.contains("/")
+                        ]
 
-                        for line, data in transcript.iterrows():
-                            match = re.search(query, data["Content"], flags)
-                            if match:
-                                before_ctx = min(match.start(), 20)
-                                match_len = match.end() - match.start()
-                                with_ctx = data["Content"][
-                                    match.start() - before_ctx : match.end() + 20
-                                ]
-                                with_ctx = (
-                                    with_ctx[:before_ctx]
-                                    + ">>>"
-                                    + with_ctx[before_ctx : before_ctx + match_len]
-                                    + "<<<"
-                                    + with_ctx[before_ctx + match_len :]
-                                )
-                                yield i, line, data, with_ctx
+            for i, (_, trans) in enumerate(transcript.iterrows()):
+                text = trans["Content"]
+                audio_start = int(trans["StTime"] * TARGET_SAMPLE_RATE)
+                audio_end = int(trans["EnTime"] * TARGET_SAMPLE_RATE)
+
+                self.samples.append(
+                    (row, subset, part, filename, text, audio_start, audio_end)
+                )
+
+                if i + 1 == max_samples_per_recording:
+                    break
+
+    def __len__(self):
+        return len(self.samples)
+
+    def _get_ix(self, ix):
+        row, subset, part, filename, text, audio_start, audio_end = self.samples[ix]
+
+        with gzip.GzipFile(
+            os.path.join(DATA_BASE_DIR, "audio", f"{subset}_{part}.tar.gz"), "r"
+        ) as gz:
+            with tarfile.TarFile(fileobj=gz) as tar:
+                with tar.extractfile(filename + ".wav") as wav_file:  # type: ignore
+                    audio = audio_bytes_to_array(wav_file.read(), SOURCE_SAMPLE_RATE)
+        audio = audio[audio_start:audio_end]
+
+        result = [text, audio]
+        if self.include_speaker_info:
+            result.append(row.to_dict())
+        if self.include_text:
+            result[0] = None
+            result.append(text)
+        return tuple(result)
+
+    def search_transcript(self, query, flags=re.IGNORECASE):
+        for i, (_, _, _, _, text, _, _) in enumerate(self.samples):
+            match = re.search(query, text, flags)
+            if match:
+                before_ctx = min(match.start(), 20)
+                match_len = match.end() - match.start()
+                with_ctx = text[match.start() - before_ctx : match.end() + 20]
+                with_ctx = (
+                    with_ctx[:before_ctx]
+                    + ">>>"
+                    + with_ctx[before_ctx : before_ctx + match_len]
+                    + "<<<"
+                    + with_ctx[before_ctx + match_len :]
+                )
+                yield i, text[match.start() : match.end()], with_ctx
 
 
 if __name__ == "__main__":
-    data = CORAALDataset(include_text=True, crop_to_first_sample=True)
+    data = CORAALDataset(include_text=True)
     print(len(data))
 
     example = next(data.search_transcript(" aks "))
